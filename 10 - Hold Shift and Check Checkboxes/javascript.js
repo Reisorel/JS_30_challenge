8c6c22322a7ac3c15db495041bfb7e0c49017616
Francois Lerosier <francois.lerosier@gmail.com>
2024-04-06 18:51:00 +0200

    day10

---
+++ 10 - Hold Shift and Check Checkboxes/javascript.js
@@ -0,0 +1,16 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+function handleCheck(e) {
+  let inBetween = false;
+if (e.shiftkey && this.checked) {
+  //
+  checkboxes.forEach(checkbox => {
+    console.log(checkbox);
+  });
+}
+
+  lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
