92967c51bb2d1e9a509231e207651fd05970ecdd
Francois Lerosier <francois.lerosier@gmail.com>
2024-04-22 23:03:16 +0200

    day14

--- 14 - JavaScript References VS Copying/javascript.js
+++ 14 - JavaScript References VS Copying/javascript.js
@@ -1,6 +1,20 @@
-    console.log("coucou");
+// start with strings, numbers and booleans
 
-    // start with strings, numbers and booleans
+let age = 100;
+let age2 = age;
+console.log(age, age2);
+age = 200;
+console.log(age, age2);
+
+let name = 'Wes';
+let name2 = name;
+console.log(name, name2);
+name = "Babidi";
+console.log(name, name2);
+
+
+
+
 
     // Let's say we have an array
     const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
